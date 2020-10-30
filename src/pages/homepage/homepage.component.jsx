a193c55a49237d8f1faf8842fbd09f83b1affadf
Hans Hofner <hhofner@gmail.com>
2020-10-30 21:30:00 +0900

    add empty component dirs and styles

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './homepage.styles.scss';
+
+const HomePage = () => (
+    <div>
+        <h1>
+            Hello on the homepage
+        </h1>
+    </div>
+);
+
+export default HomePage;
