adf2eb91b6e6a32feaba4ceb953b60c1a7d69980
Hans Hofner <hhofner@gmail.com>
2020-12-21 12:09:01 +0900

    add item to cart util function

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import CustomButtom from "../custom-button/custom-buttom.component";
 
+import { connect } from 'react-redux';
+import { addItem } from "../../redux/cart/cart.actions";
+
 import './collection-item.styles.scss';
 
-const CollectionItem = ({id, name, price, imageUrl}) => (
-    <div className="collection-item">
-        <div
-            className="image"
-            style={{
-                backgroundImage: `url(${imageUrl})`
-        }}/>
-        <div className="collection-footer">
-            <span className="name"> { name } </span>
-            <span className="price"> { price }</span>
+const CollectionItem = ({item, addItem}) => {
+    const { name, price, imageUrl } = item;
+    return (
+        <div className="collection-item">
+            <div
+                className="image"
+                style={{
+                    backgroundImage: `url(${imageUrl})`
+                }}/>
+            <div className="collection-footer">
+                <span className="name"> {name} </span>
+                <span className="price"> {price}</span>
+            </div>
+            <CustomButtom onClick={() => addItem(item)}> Add to cart </CustomButtom>
         </div>
-        <CustomButtom> Add to cart </CustomButtom>
-    </div>
-);
+    );
+}
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -6,5 +6,10 @@ export const toggleCartHidden = () => ({
     //The payload value is optional.
 })
 
+export const addItem = item => ({
+    type: CartActionTypes.ADD_ITEM,
+    payload: item
+})
+
 // Remember (!) An "action" is just an object with a `type`, and a `payload`.
 // And an action creator is a method that will make an action for you.
